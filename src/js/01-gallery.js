1efed8e0208e3aad910efaa7267abfadf38f0378
annamelya2021 <anna.basyuk@gmail.com>
2022-01-31 18:03:42 +0200

    copy from 7 and add npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery')
+
+
+const galleryMarkup = galleryItems.map(galleryItem => {
+    return `
+<a class="gallery__item" href="${galleryItem.original}">
+    <img
+    class="gallery__image"
+    src="${galleryItem.preview}"
+    alt="${galleryItem.description}"
+    />
+</a>
+`
+
+})
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup.join(""))
+
+new SimpleLightbox('.gallery a', { /* options */
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition : 'bottom',
+});
