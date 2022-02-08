c0b35a8b191e9b2c0b79d608f28482fe7b67eebe
annamelya2021 <anna.basyuk@gmail.com>
2022-02-08 14:40:00 +0200

    add solved tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,73 @@
 
+
+import Player from '@vimeo/player';
+import { throttle }  from 'lodash';
+
+const videoPlayerRef = document.querySelector("iframe")
+const player = new Player(videoPlayerRef)
+console.dir(videoPlayerRef);
+// паттерн магические числа и строки
+const KEY = "videoplayer-current-time"
+// const onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
+const onPlay = function (data) {
+// получаем значение
+    const time = data.seconds
+    console.log(time);
+// значение из инпута всегда строка стригифаим только масивы и обьекты
+// сохраняем  значение  в хранилище
+    localStorage.setItem(KEY , time)
+};
+
+const throtlleFunction = throttle(onPlay, 1000)
+
+player.on('timeupdate', throtlleFunction);
+//    player.on('play', function() {
+        // console.log('played the video!');
+//     });
+
+// // получаем значение из локал стореджа
+const getStorageData = localStorage.getItem(KEY)
+// при перезагрузке страницы
+//  если getStorageData приводится к тру, то тогда мы можем с ним работать
+// если ключа нет вернет null  но ничего не выполнится
+if (getStorageData) {
+    //  если да, плеер сохранни поченные значения из локалстореджа
+     player.setCurrentTime(getStorageData)
+}
+
+
+
+
+
+
+
+//  if (localStorage.getItem("videoplayer-current-time")){
+//      player.setCurrentTime(localStorage.getItem("videoplayer-current-time" ))
+// }
+
+
+
+//     <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
+
+// <script src="https://player.vimeo.com/api/player.js"></script>
+// <script>
+//     const iframe = document.querySelector('iframe');
+//     const player = new Vimeo.Player(iframe);
+
+//     player.on('play', function() {
+//         console.log('played the video!');
+//     });
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
+// </script>
+
+// on(event: string, callback: function): void
+// Add an event listener for the specified event. Will call the callback with a single parameter, data, that contains the data for that event. See events below for details.
+
+
+
+// player.on('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,73 @@
+import throttle from 'lodash.throttle';
+// паттерн магические числа и строки
+const STORAGE_KEY = 'feedback-msg'
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('textarea')
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+// добавляем throttle
+refs.textarea.addEventListener('input',throttle(onTextareaInput, 1000));
+let formData = {};
+populateTextarea()
+
+// refs.form.addEventListener('input', evt => {
+//     // formData = {};
+//   // console.log(evt.target.value);
+//   // console.log(evt.target.name)
+//   formData[evt.target.name] = evt.target.value;
+//   //если такого влюча еще не будет в этом объекте он создастся
+//   // если будет его значение перезапишется
+//   // реализация собственног локалсторедж
+//   console.log(formData)
+
+// });
+
+function onFormSubmit(evt) {
+  // Останавливаем поведение по умолчанию
+  evt.preventDefault();
+  console.log('отправляем форму');
+  // console.log(formData);
+  // formData = {};
+  // очищаем форму
+  evt.currentTarget.reset();
+  // очищаем в сторедже и текстерия при сабмите
+  localStorage.removeItem(STORAGE_KEY)
+  // не clear он очищает все
+ }
+
+function onTextareaInput(evt) {
+  // получаем значение поля
+  // currentTarget всплывает и меняется поэтому используем target
+   formData[evt.target.name] = JSON.stringify(formData);
+  //если такого влюча еще не будет в этом объекте он создастся
+  // если будет его значение перезапишется
+  // реализация собственног локалсторедж
+  const savedData = evt.target.value;
+  console.log(formData);
+  // сохраняем его в хранилище
+   localStorage.setItem(STORAGE_KEY, savedData)
+}
+
+
+// получаем значение из хранилища
+// если там чт-то было обновляем Dom
+function populateTextarea(evt) {
+  // при перезагрузке страницы
+  // получаем значение из локал стореджа
+  const saveMessage = localStorage.getItem(STORAGE_KEY)
+  console.log(saveMessage);
+  // если ключа нет вернет null но ничего не выполнится
+  if (saveMessage) {
+     //  если да, сохранни поченные значения из локалстореджа
+    console.log(saveMessage)
+    const parseSave = JSON.parse(saveMessage)
+    console.log( parseSave);
+    // если в поле textarea пусто ничего не произойдет
+    // если в поле что-то есть оно сохранится в локалсторедж
+    // и згачение из локала при перезагружке будет находиться в поле тексериа
+    refs.textarea.value = parseSave;
+  }
+}
